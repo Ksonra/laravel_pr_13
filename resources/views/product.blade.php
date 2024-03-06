c640f0c1f77009c4bec7bdf68dfca82ea02c13fb
Ksonra <116746086+Ksonra@users.noreply.github.com>
2024-03-06 21:12:47 +0300

    fixx

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -6,23 +6,30 @@
             <a href="{{ asset('catalog/' . $product->catalog->id) }}"> <-{{ $product->catalog->name }} <-
                     {{ $product->name }} </a></h2>
 
-
-
-
-
-                    <section class="product" id="product">
-                        <div class="box-container">
-                            <div class="box">
-                                {!! $product->body !!}
-                                <img src="{{ asset('/storage/' . $product->picture) }}" alt="">
+                    <div class="container_blogs">
+                        <div class="image-block_blogs">
+                            <img src="{{ asset('/storage/' . $product->picture) }}" alt="">
+                        </div>
+                        <div class="content-block_blogs">
+                            <div class="content_news">
+                                <h3>{{ $product->name }}</h3>
+                                <br>
+                                <div>
+                                    <h2>{!! $product->body !!}</h2>
+                                </div>
+                                <div>
+                                    @if ($product->discount)
+                                        <h2>{{ $product->price - ($product->price * $product->discount) / 100 }}$</h2>
+                                    @else
+                                        <h3 class="price">{{ $product->price }}$</h3>
+                                    @endif
+                                </div>
                             </div>
-
-                                <div class="icons">
+                                <div class="icons_one">
                                     <a href="#" class="fas fa-shopping-cart"></a>
                                     <a href="#" class="fas fa-heart"></a>
                                 </div>
-
                         </div>
-                    </section>
+                    </div>
     </section>
 @endsection
